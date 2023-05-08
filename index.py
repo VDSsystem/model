0eabf974b0b7fd1a6d465237c462a40f300621a3
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-08 19:17:15 +0300

    updates

--- index.py
+++ index.py
@@ -1,5 +1,10 @@
-from flask import Flask 
+from flask import Flask
+
 app = Flask(__name__)
+
 @app.route("/")
 def home():
-    return "<h2>Hello!</h2"
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
