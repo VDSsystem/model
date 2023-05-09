cf4a970637b0c64252832d1a9213469a4faeccb8
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-09 21:34:59 +0300

    idk

--- api/index.py
+++ api/index.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'POST':
+        id = request.json['id']
         url = request.json['url']
         print(url)
-        return "Received URL: " + str(url)
+        return "Received URL: " + str(id) + str(url)
     else:
         return "Hello, World!"
