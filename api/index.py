0a6e33f8c44265fba864a0b6d2187d7aab127392
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-09 21:01:24 +0300

    new

--- api/index.py
+++ api/index.py
@@ -1,10 +1,6 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route("/")
-def home():
+@app.get('/')
+def hello_world():
     return "Hello, World!"
-
-if __name__ == "__main__":
-    app.run(debug=True)
