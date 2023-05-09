245081550b099d46af77396589a761c9e896c5d5
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-09 21:33:06 +0300

    IDK

--- api/index.py
+++ api/index.py
@@ -1,19 +1,11 @@
 from flask import Flask, request
-from pymongo import MongoClient
-
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://vdssystem0:<VA123AVdkd>@cluster0.vdo3rvu.mongodb.net/test?retryWrites=true&w=majority"
-mongo = MongoClient(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'POST':
-        id = request.json['id']
-        doc = mongo.test.images.find_one({'id': id})
-        if doc:
-            url = doc['url']
-            return "URL for ID " + str(id) + ": " + url
-        else:
-            return "No URL found for ID " + str(id)
+        url = request.json['url']
+        print(url)
+        return "Received URL: " + str(url)
     else:
         return "Hello, World!"
