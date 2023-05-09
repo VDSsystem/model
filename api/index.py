a89a9c8032abf73041ad7a3aa02814856b54e832
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-09 21:29:32 +0300

    final

--- api/index.py
+++ api/index.py
@@ -1,16 +1,15 @@
 from flask import Flask, request
-from pymongo import MongoClient
+from flask_pymongo import MongoClient
 
 app = Flask(__name__)
-client = MongoClient('mongodb+srv://vdssystem0:<VA123AVdkd>@cluster0.vdo3rvu.mongodb.net/test?retryWrites=true&w=majority')
-db = client.test
-collection = db.images
+app.config["MONGO_URI"] = "mongodb+srv://vdssystem0:<VA123AVdkd>@cluster0.vdo3rvu.mongodb.net/test?retryWrites=true&w=majority"
+mongo = MongoClient(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'POST':
         id = request.json['id']
-        doc = collection.find_one({'id': id})
+        doc = mongo.test.images.find_one({'id': id})
         if doc:
             url = doc['url']
             return "URL for ID " + str(id) + ": " + url
