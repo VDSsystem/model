9119e1ff577fb7b04915ed67a9854f4e6d5bd88a
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-09 22:46:23 +0300

    IRDK

--- api/index.py
+++ api/index.py
@@ -1,4 +1,9 @@
 from flask import Flask, request
+from flask_pymongo import pymongo
+CONNECTION_STRING = "mongodb+srv://vdssystem0:<VA123AVdkd>@cluster0.vdo3rvu.mongodb.net/?retryWrites=true&w=majority"
+client = pymongo.MongoClient(CONNECTION_STRING)
+db = client.get_database('test')
+user_collection = pymongo.collection.Collection(db, 'images')
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -7,6 +12,7 @@ def hello_world():
         id = request.json['id']
         url = request.json['url']
         print(url)
-        return "Received URL: " + str(id) + str(url)
+        db.db.collection.insert_one({"id": "url"})
+        return "Connected to the data base!"
     else:
         return "Hello, World!"
