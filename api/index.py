a83b4649c6222bbd9f813c9c955d047be959d868
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-09 22:48:51 +0300

    Chsnge

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_pymongo import pymongo
-CONNECTION_STRING = "mongodb+srv://vdssystem0:<VA123AVdkd>@cluster0.vdo3rvu.mongodb.net/?retryWrites=true&w=majority"
+CONNECTION_STRING = "mongodb+srv://vdssystem0:<VA123AVdkd>@cluster0.vdo3rvu.mongodb.net/test?retryWrites=true&w=majority"
 client = pymongo.MongoClient(CONNECTION_STRING)
 db = client.get_database('test')
 user_collection = pymongo.collection.Collection(db, 'images')
