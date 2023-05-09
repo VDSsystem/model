fb57d2695d97faed9e95e604d3d9bfcada347561
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-09 23:41:01 +0300

    Finalll

--- api/index.py
+++ api/index.py
@@ -1,7 +1,8 @@
-from flask import Flask, request
-import requests
-import tempfile
 import os
+import tempfile
+import requests
+
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -16,9 +17,10 @@ def hello_world():
         # Download the image from the URL
         image_response = requests.get(data['url'])
         image_extension = '.' + image_response.headers['Content-Type'].split('/')[-1]
-        temp_fd, image_path = tempfile.mkstemp(suffix=image_extension)
-        with os.fdopen(temp_fd, 'wb') as temp_file:
-            temp_file.write(image_response.content)
+        with tempfile.TemporaryDirectory() as temp_dir:
+            image_path = os.path.join(temp_dir, f"image{image_extension}")
+            with open(image_path, 'wb') as temp_file:
+                temp_file.write(image_response.content)
 
         return f"Image downloaded to {image_path}"
     else:
