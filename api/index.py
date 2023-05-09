c6bb1a5a36c7fa178dfa599457c9200a66378526
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-09 23:01:34 +0300

    A*a89

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_pymongo import pymongo
 CONNECTION_STRING = "mongodb+srv://vdssystem0:<VA123AVdkd>@cluster0.vdo3rvu.mongodb.net/test?retryWrites=true&w=majority"
 client = pymongo.MongoClient(CONNECTION_STRING)
@@ -11,8 +11,12 @@ def hello_world():
     if request.method == 'POST':
         id = request.json['id']
         url = request.json['url']
-        print(url)
         user_collection.insert_one({"id": id, "url": url})
-        return "Connected to the data base!"
+        return "Connected to the database!"
     else:
-        return "Hello, World!"
+        all_data = []
+        for item in user_collection.find():
+            item.pop('_id')
+            all_data.append(item)
+        return jsonify(all_data)
+
