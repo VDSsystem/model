504146ff149a5a075cba9889d6eee3da3f172312
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-09 23:11:19 +0300

    eeeg

--- api/index.py
+++ api/index.py
@@ -1,9 +1,8 @@
 from flask import Flask, request, jsonify
-from flask_pymongo import pymongo
-CONNECTION_STRING = "mongodb+srv://vdssystem0:<VA123AVdkd>@cluster0.vdo3rvu.mongodb.net/test?retryWrites=true&w=majority"
-client = pymongo.MongoClient(CONNECTION_STRING)
-db = client.get_database('test')
-user_collection = pymongo.collection.Collection(db, 'images')
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://vdssystem0:<VA123AVdkd>@cluster0.vdo3rvu.mongodb.net/test?retryWrites=true&w=majority')
+db = client.Cluster0
+people = db.images
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -11,11 +10,11 @@ def hello_world():
     if request.method == 'POST':
         id = request.json['id']
         url = request.json['url']
-        user_collection.insert_one({"id": id, "url": url})
+        people.insert_one({"id": id, "url": url})
         return "Connected to the database!"
     else:
         all_data = []
-        for item in user_collection.find():
+        for item in people.find():
             item.pop('_id')
             all_data.append(item)
         return jsonify(all_data)
