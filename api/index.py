6d9f5321d63392c713560243f44d7531bdb8363e
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-09 20:46:02 +0300

    postMethod

--- api/index.py
+++ api/index.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
-@app.get('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return "Hello, World!"
+    if request.method == 'POST':
+        id = request.json.get('id')
+        print(f"Received ID: {id}")
+        return "Received ID"
+    else:
+        return "Hello, World!"
