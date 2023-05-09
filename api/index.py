d4f26b6eeebb8d8c340e3b32741976638d4f7d8e
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-09 20:52:33 +0300

    new

--- api/index.py
+++ api/index.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.get('/')
 def hello_world():
     return "Hello, World!"
+
+@app.route('/id', methods=['POST'])
+def print_id():
+    data = request.get_json()
+    id = data.get('id')
+    print(f'The received id is: {id}')
+    return 'ID received and printed to console.'
