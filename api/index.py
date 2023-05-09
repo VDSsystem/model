3fba584829bc46b0bc9da7a7bcdb27fb4bd72505
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-09 23:22:21 +0300

    I wanna crt

--- api/index.py
+++ api/index.py
@@ -1,21 +1,14 @@
-from flask import Flask, request, jsonify
-from pymongo import MongoClient
-client = MongoClient('mongodb+srv://vdssystem0:<VA123AVdkd>@cluster0.vdo3rvu.mongodb.net/test?retryWrites=true&w=majority')
-db = client.Cluster0
-people = db.images
+from flask import Flask, request
+import requests
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'POST':
         id = request.json['id']
-        url = request.json['url']
-        people.insert_one({"id": id, "url": url})
-        return "Connected to the database!"
+        url = f"https://vadss.vercel.app/api/savedImages?id={id}"
+        response = requests.get(url)
+        data = response.json()
+        return f"Image URL: {data['url']}"
     else:
-        all_data = []
-        for item in people.find():
-            item.pop('_id')
-            all_data.append(item)
-        return jsonify(all_data)
-
+        return "Hello, World!"
