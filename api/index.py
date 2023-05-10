6f0d16185fd2bfac4ede059fd57f5da78a46d23d
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-11 02:54:37 +0300

    finn

--- api/index.py
+++ api/index.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 import requests
 from flask_cors import CORS
-import subprocess
 
 app = Flask(__name__)
 cors = CORS(app, resources={r'*': {'origins': '*'}})
